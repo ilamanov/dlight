6c5972688aa064ca14f87018d607c30ec219d5cd
Nazar <nazar.ilamanov@gmail.com>
2020-07-07 11:03:02 -0400

    added links to the article

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # dlight
+
 Tools for dissection/visualization of ConvNets.
 
-This is the code accompanying the article titled "Explainable MNIST classification: dissection of a ConvNet" which can be found at (TODO add link).
+This is the code accompanying the article titled ["Explainable MNIST classification: dissection of a ConvNet"](https://towardsdatascience.com/explainable-mnist-classification-dissection-of-a-convnet-f32910d52842).
 
 See this [Colab notebook](https://colab.research.google.com/drive/1GqynTl2NhVPMUk3LCOQ91yXGsLf1UmJj?usp=sharing) for example usage of the code.
 
--- dlight/dissect/conv.py
+++ dlight/dissect/conv.py
@@ -8,7 +8,7 @@ import dlight
 
 
 def show_conv_dissection(input_to_conv, node, outer_idx, input_description=None):
-    """ Conv dissection consists of the following columns (TODO see medium article for definition of each column, also which section):
+    """ Conv dissection consists of the following columns
         - input to convolutional layer
         - (optional) description of the input
         - index of each input to convolutional layer
@@ -16,6 +16,8 @@ def show_conv_dissection(input_to_conv, node, outer_idx, input_description=None)
         - intermediate activations 
         - cumulative activations
         - output
+        (See the figure title "Dissected activation of conv2_3" in https://towardsdatascience.com/explainable-mnist-classification-dissection-of-a-convnet-f32910d52842
+        for the definition of each column.)
 
     Args:
         input_to_conv ([Tensor]): expected shape [B, C, H, W]
